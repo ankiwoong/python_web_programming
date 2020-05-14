6d02f5a58938455c4d04d568f91b871a3c7abec8
AnKiWoong <ankiwoong@gmail.com>
2020-05-14 21:39:26 +0900

    URLconf 코딩

---
+++ polls/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+app_name = 'polls'
+
+urlpatterns = [
+    # /polls/
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail,
+         name='detail'),                # /polls/5/
+    path('<int:question_id>/results/', views.results,
+         name='results'),      # /polls/5/result/
+    path('<int:question_id>/vote/', views.vote,
+         name='vote'),               # /polls/5/vote/
+]
