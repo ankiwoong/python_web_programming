d445ab389b800820f05ae9647a16a45183419b32
ankiwoong <ankiwoong@gmail.com>
2020-05-15 14:16:11 +0900

    작업 확인 및 디버깅 작업

--- polls/models.py
+++ polls/models.py
@@ -6,7 +6,7 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
-        return self.question.question_text
+        return self.question_text
 
 
 class Choice(models.Model):
@@ -14,5 +14,5 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    def __str__(str):
+    def __str__(self):
         return self.choice_text
