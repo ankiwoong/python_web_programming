f06631913acd3f33b0fb33a4e411deae379bea7b
ankiwoong <ankiwoong@gmail.com>
2020-05-15 16:26:20 +0900

    Question 및 Choice를 한 화면에서 변경하기

--- polls/admin.py
+++ polls/admin.py
@@ -3,12 +3,24 @@ from django.contrib import admin
 from polls.models import Question, Choice
 
 
+# StackedInline 방식
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+# TabularInline 방식
+# class ChoiceInline(admin.TabularInline):
+#     model = Choice
+#     extra = 2
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Question Statement', {'fields': ['question_text']}),
         ('Date Information', {'fields': [
          'pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]        # Choice 모델 클래스 같이 보기
 
 
 admin.site.register(Question, QuestionAdmin)
