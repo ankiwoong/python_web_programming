edb9bd2e83e558c62eea985be9de5814db9b7031
ankiwoong <ankiwoong@gmail.com>
2020-05-15 14:49:50 +0900

    필드 순서 변경하기

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from polls.models import Question, Choice
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']      # 필드 순서 변경
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
