b944c5c831ad02ef4f49cbc95ce1988a719980ec
AnKiWoong <ankiwoong@gmail.com>
2020-05-15 20:10:37 +0900

    list_filter 필터

--- polls/admin.py
+++ polls/admin.py
@@ -22,6 +22,7 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInline]                            # Choice 모델 클래스 같이 보기
     list_display = ('question_text', 'pub_date')        # 레코드 리스트 컬럼 지정
+    list_filter = ['pub_date']                          # 필터 사이드 바 추가
 
 
 admin.site.register(Question, QuestionAdmin)
