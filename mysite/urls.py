a35bbaab45fca77ca85bd4d085bda72db4ccbc38
AnKiWoong <ankiwoong@gmail.com>
2020-05-17 09:05:33 +0900

    URLconf 코딩하기

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from mysite import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomeView.as_view(), name='home'),     # 추가
     path('polls/', include('polls.urls')),
     path('books/', include('books.urls')),      # 추가
 ]
