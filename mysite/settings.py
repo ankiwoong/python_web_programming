9e9cab27e4e5ac6572771f9418cd75f712d1ea1b
AnKiWoong <ankiwoong@gmail.com>
2020-05-17 09:36:44 +0900

    apps.py 활용 맛보기

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',               # 추가(애플리케이션의 설정 클래스로 등록)
-    'books.apps.BooksConfig',               # 추가
+    # 'books.apps.BooksConfig',               # 추가
+    'books',
 ]
 
 MIDDLEWARE = [
